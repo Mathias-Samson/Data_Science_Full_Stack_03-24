6c708a5c00f9b08072c1715ca218663b8391628b
Mathias_Samson <mathias.samson91@gmail.com>
2024-07-06 19:17:48 +0200

    Stramlit app finished

--- 07_DEPLOYMENT_Getaround/Streamlit_server/README.md
+++
@@ -1,4 +0,0 @@
-# Getaround_app
-
-Set the env variable:
-- NIXTLA_API_KEY=
--- 07_DEPLOYMENT_Getaround/Streamlit_server/app.py
+++ 07_DEPLOYMENT_Getaround/Streamlit_server/app.py
@@ -1,28 +1,28 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px 
+import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 import requests
 
 ### Config
 st.set_page_config(
-    page_title="Getaround analysis",
-    page_icon="🚗 ",
+    page_title="Getaround",
+    page_icon="🚗",
     layout="wide"
 )
 
 ### App
-st.title("🚗 Getaround analysis")
+st.title("🚗 Getaround app")
 
 st.markdown("""
-    You'll find below elements to design the "delay before rebooking" getaround feature 👇
+    You will find in this dashboard below info about the "delay before rebooking"
 """)
 
 st.markdown("---")
 
 
-# Use `st.cache` when loading data is extremly useful
+# Use `st.cache_data` when loading data is extremely useful
 # because it will cache your data so that your app 
 # won't have to reload it each time you refresh your app
 @st.cache_data
@@ -35,138 +35,202 @@ def load_data():
 
 data_load_state = st.text('Loading data...')
 dataset = load_data()
-data_load_state.text("") # change text from "Loading data..." to "" once the the load_data function has run
+data_load_state.text("") # change text from "Loading data..." to "" once the load_data function has run
 
-df_no_canceled = dataset.loc[dataset['state'] != "canceled"]
-df_no_canceled['late_by'] = df_no_canceled['delay_at_checkout_in_minutes'].apply(lambda x : max(x,0))
+# Calculate proportions
+mobile_rentals = dataset[dataset["checkin_type"] == "mobile"]
+prop_mobile = (dataset["checkin_type"] == "mobile").sum() / len(dataset) * 100
+connect_rentals = dataset[dataset["checkin_type"] == "connect"]
+prop_connect = (dataset["checkin_type"] == "connect").sum() / len(dataset) * 100
 
-latencies = df_no_canceled['late_by'].value_counts().reset_index()
-overtime = pd.DataFrame(columns=["nb_late","percent_late"])
+prop_two_rentals = (dataset["previous_ended_rental_id"] > 0).sum() / len(dataset) * 100
+two_or_more_rentals = dataset[dataset["previous_ended_rental_id"] > 0]
 
-df_no_canceled_short_delay = df_no_canceled.loc[df_no_canceled['time_delta_with_previous_rental_in_minutes'].notna()]
-df_no_canceled_short_delay['late_by'].count(), df_no_canceled['late_by'].count()
 
-df_no_canceled_short_delay['real_delta'] = df_no_canceled_short_delay['time_delta_with_previous_rental_in_minutes'] - df_no_canceled_short_delay['late_by']
-real_latencies = df_no_canceled_short_delay['real_delta'].value_counts().reset_index()
-
-#real_filtered_latencies = real_latencies.loc[(real_latencies["real_delta"] < 0) & (real_latencies["real_delta"] > -180)]
-
-col1, col2, col3 = st.columns(3)
-col1.metric("Total bookings", f"{dataset.shape[0]}")
-col2.metric("Uncanceled rentals", f"{df_no_canceled.shape[0]}")
-col3.metric("Next booking under 12h", f"{df_no_canceled_short_delay.shape[0]}")
+# Display metrics
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Total car reservations", dataset.shape[0])
+col2.metric("Percentage of mobile checkin", f"{prop_mobile.round(1)}%")
+col3.metric("Proportion of connect checkin", f"{prop_connect.round(1)}%")
+col4.metric("Proportion of rebooked cars", f"{prop_two_rentals.round(1)}%")
 
 st.markdown("---")
 
 st.header("""
-    What's the volume of rentals ending with delays ?
+    Statistics about delay and time delta between two rentals 🕐
 """)
 
-#### Create two columns
-col1, col2 = st.columns(2)
+# Create two columns
+col1, col2 = st.columns([2,8])
 
 with col1:
-    st.markdown("**1️⃣ Late vs booking**")
-
-    for delay in range (6):
-        filtered_latencies = latencies.loc[latencies["late_by"] > (60*delay)]
-        overtime.loc[delay,"nb_late"] = filtered_latencies['count'].sum()
-        overtime.loc[delay,"percent_late"] = round(100 * filtered_latencies['count'].sum() / df_no_canceled['late_by'].count(), 1)
-
-    fig = px.area(overtime, y="nb_late")
-    fig.update_layout(
-        xaxis_title="hours late"
-    )
-    #fig.show()
-    st.plotly_chart(fig, use_container_width=True)
-
-    fig = px.area(overtime, y="percent_late")
-    fig.update_layout(
-        xaxis_title="hours late"
-    )
-    #fig.show()
-    st.plotly_chart(fig, use_container_width=True)
+    prop_late_drivers = (dataset["delay_at_checkout_in_minutes"] > 0).sum() / len(dataset) * 100   
+    prop_mobile_late = (mobile_rentals["delay_at_checkout_in_minutes"] > 0).sum() / len(mobile_rentals) * 100 
+    prop_connect_late = (connect_rentals["delay_at_checkout_in_minutes"] > 0).sum() / len(connect_rentals) * 100 
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown(f"""
+        <div style="text-align: center;">
+            <p>Percentage of late drivers</p>
+            <h3> {prop_late_drivers.round(1)}%</h3>
+        </div>
+    """, unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown(f"""
+        <div style="text-align: center;">
+            <p>Percentage of late mobile drivers</p>
+            <h3> {prop_mobile_late.round(1)}%</h3>
+        </div>
+    """, unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown(f"""
+        <div style="text-align: center;">
+            <p>Percentage of late connect drivers</p>
+            <h3> {prop_connect_late.round(1)}%</h3>
+        </div>
+    """, unsafe_allow_html=True)
 
 with col2:
-    st.markdown("**1️⃣ Late vs next rental**")
+    fig_hist = go.Figure()
 
-    for delay in range (6):
-        real_filtered_latencies = real_latencies.loc[(real_latencies["real_delta"] < 0) & (real_latencies["real_delta"] < (-60*delay))]
-        overtime.loc[delay,"nb_real_late"] = real_filtered_latencies['count'].sum()
-        overtime.loc[delay,"percent_real_late"] = round(100 * real_filtered_latencies['count'].sum() / df_no_canceled['late_by'].count(), 1)
+    fig_hist.add_trace(go.Histogram(
+        x=mobile_rentals["time_delta_with_previous_rental_in_minutes"],
+        name='Mobile',
+        marker_color='blue',
+        opacity=0.75
+    ))
 
-    fig = px.area(overtime, y="nb_real_late")
-    fig.update_layout(
-        xaxis_title="hours late"
+    fig_hist.add_trace(go.Histogram(
+        x=connect_rentals["time_delta_with_previous_rental_in_minutes"],
+        name='Connect',
+        marker_color='red',
+        opacity=0.75
+    ))
+
+    fig_hist.update_layout(
+        title={
+            'text': 'Distribution of Time Delta vs next rental (scope)',
+            'x': 0.5,
+            'xanchor': 'center'
+        },
+        xaxis_title='Time Delta (minutes)',
+        yaxis_title='Transaction qty',
+        barmode='stack',
+        height=500,
+        legend=dict(
+            x=1,
+            y=0.5,
+            xanchor='left',
+            yanchor='middle',
+            orientation='v'
+        )
     )
-    #fig.show()
-    st.plotly_chart(fig, use_container_width=True)
 
-    fig = px.area(overtime, y="percent_real_late")
-    fig.update_layout(
-        xaxis_title="hours late"
-    )
-    #fig.show()
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig_hist)
 
+st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("""
-    More than 50% of rentals are brought back late.
-    Given the dataset, we can't know whether these late drop off have been discussed with the car owner or not.
+    Almost 50% of rentals are brought back late.
+    It is hard to know at what point the owner has knowledge and agrees with the delay.
             
-    We should look at the "real late" drop off, meaning the drop off made after the start time for the next rental.
-            
-    We can see more than 15% of rental are deposited later than the next rental. The issue raised by Getaround looks serious.
+    We can notice on another hand that the time delta between two rentals is often less than 2 hours,
+    which is critical in case of delay from the previous renter.
 """)
 
 st.markdown("---")
 
 st.header("""
-    What's the volume of bookings impacted by a "Delay before next rental" feature ?
+    What is the impact of delay on rentals and cancellations?
 """)
 
-df_no_canceled_hours_delay = df_no_canceled_short_delay.loc[df_no_canceled_short_delay['time_delta_with_previous_rental_in_minutes'] < 300]
-df_no_canceled_mins_delay = df_no_canceled_short_delay.loc[df_no_canceled_short_delay['time_delta_with_previous_rental_in_minutes'] < 90]
+# Create two columns for delay impact
+col1, col2 = st.columns([2,8])
 
-col1, col2, col3 = st.columns(3)
+with col1:
+    # Delay impact on rentals
+    delay_delay = (two_or_more_rentals["delay_at_checkout_in_minutes"] > two_or_more_rentals["time_delta_with_previous_rental_in_minutes"]).sum() / len(two_or_more_rentals) * 100
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown(f"""
+        <div style="text-align: center;">
+            <p>Rentals brought after<br>next rental beggining</p>
+            <h2>{delay_delay.round(1)}%</h2>
+        </div>
+    """, unsafe_allow_html=True)
+    
+    problematic_cases = (two_or_more_rentals["state"] == "canceled").sum() / len(two_or_more_rentals) * 100
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown(f"""
+        <div style="text-align: center;">
+            <p>Percentage of canceled rentals<br>due to delay</p>
+            <h2>{problematic_cases.round(1)}%</h2>
+        </div>
+    """, unsafe_allow_html=True)
 
-col1.metric("Next booking under 6h", 
-          f"{df_no_canceled_hours_delay.shape[0]}", 
-          f"({round(100* df_no_canceled_hours_delay['real_delta'].count() / df_no_canceled['late_by'].count(), 2)} %)")
-col2.metric("Next booking under 90min", 
-          f"{df_no_canceled_mins_delay.shape[0]}", 
-          f"({round(100* df_no_canceled_mins_delay['real_delta'].count() / df_no_canceled['late_by'].count(), 2)} %)")
-col3.metric("90min / 6h delays", 
-          f"{round(100*df_no_canceled_mins_delay.shape[0]/df_no_canceled_hours_delay.shape[0],1)} %",)
+with col2:
+    # Plot cumulative frequency
+    dataset_pb = two_or_more_rentals.loc[two_or_more_rentals["state"] == "canceled"]
+    mobile_pb = dataset_pb[dataset_pb["checkin_type"] == "mobile"]
+    connect_pb = dataset_pb[dataset_pb["checkin_type"] == "connect"]
 
-fig = px.histogram(df_no_canceled_hours_delay, y='time_delta_with_previous_rental_in_minutes', nbins=6)
-#fig.show()
-st.plotly_chart(fig, use_container_width=True)
+    def plot_cumulative_frequency(data, label, color):
+        sorted_data = np.sort(data)
+        cumul_counts = np.arange(1, len(sorted_data) + 1)
+        return go.Scatter(x=sorted_data, y=cumul_counts, mode='lines', name=label, line=dict(color=color))
+
+    fig_cumulative = go.Figure()
+
+    fig_cumulative.add_trace(plot_cumulative_frequency(two_or_more_rentals['time_delta_with_previous_rental_in_minutes'], 'Total', 'white'))
+    fig_cumulative.add_trace(plot_cumulative_frequency(mobile_rentals['time_delta_with_previous_rental_in_minutes'], 'Mobile', 'blue'))
+    fig_cumulative.add_trace(plot_cumulative_frequency(connect_rentals['time_delta_with_previous_rental_in_minutes'], 'Connect', 'green'))
+    fig_cumulative.add_trace(plot_cumulative_frequency(mobile_pb['time_delta_with_previous_rental_in_minutes'], 'Mobile Cancelled', 'orange'))
+    fig_cumulative.add_trace(plot_cumulative_frequency(connect_pb['time_delta_with_previous_rental_in_minutes'], 'Connect Cancelled', 'red'))
+
+    fig_cumulative.update_layout(
+        title={
+            'text': 'Cumulative amount of transactions with a given Time Delta Between Rentals (300min max)',
+            'x': 0.5,
+            'xanchor': 'center'
+        },
+        xaxis_title='Time Delta (minutes)',
+        yaxis_title='Cumulative transaction qty',
+        xaxis=dict(range=[0, 300]),
+        yaxis=dict(range=[0, 1000]),
+        legend=dict(
+            x=1,
+            y=0.5,
+            xanchor='left',
+            yanchor='middle',
+            orientation='v'
+        )
+    )
+
+    st.plotly_chart(fig_cumulative)  
 
 st.markdown("""
-    We can see 5.6% of uncanceled rentals start less than 6h after the previous rental's drop off time.
-    Decreasing the threshold to rentals starting less than 90min after the previous rental's drop off time, we get 3% of all uncanceled rentals
-    (it's still more than 50% of the 6h threshold)
+We can see that in the case where the owner have a second reservation for its car,
+around 15% of the cars are deposited later than the time where next rental beggins.
+When it happens, the next renter cancels its reservation very often.
+There is not a big difference between the type of connection for this phenomenon.
 """)
 
-st.subheader("3% of rentals start less than 90min after the previous rental")
+st.markdown("---")
 
+st.header("""
+    Analysis
+""") 
+st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("""
-    Given "real late" dropoff less than 60min for the next rental reprents 0.8% of rentals (50% of all real late dropoffs)
-    Setting a 90min delay between two bookings would make sure these 0.8% rentals happen smoothly.
-    On the other side, 3% of all rentals would be affected.
+    The issue seams serious, even if the proportion of rebooked cars is low, 
+    when it is the case more than 10% of cancels due to delay is something to improve.
+    
+
+    The problem is that a almost a third of rebooked cars have less than 2hrs delta between the twor rentals.
+    It is a risk to loose these transactions because of a potential gain that represent approx 10% of the transaction volume.       
             
-    While this could mean a delay is a bad idea : gain 0.8% to loose 3%, avoiding these specific 3% bookings does NOT mean
-    a loss of 3% of bookings. It just means "not showing these booking options", leading users to others booking options.
-            
-    While it's not possible with the given data to evaluate the impact of such a feature, we could design a smarter feature 
-    which would :  
-    - Remove the risk of loosing bookings 
-    - Reduce the risk of "real late" dropoffs
-    - Reduce the client insatisfaction on the rare case it still happens
-    - Provide new data to deepen the analysis
-            
-    The feature would be:
-    - Short delay with the previous rental are flagged
-    - A warning is displayed to the user about the short delay risk 
-    - Short delay booking options are penalized in the search engine
+    We cannot evaluate the financial impact for the owners with our dataset, but we could maybe try a different approach :  
+    * Warning the next renter when he chooses a time to pick up the car too close from the return 
+    * Penalising renters that allow low delta times between rentals
 """)
